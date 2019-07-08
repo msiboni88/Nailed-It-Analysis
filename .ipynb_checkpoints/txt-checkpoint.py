28defe0556eb00a3f84010c901fd841d0bab7dbf
Marguerite Siboni <mags@Marguerites-MacBook-Air.local>
2019-07-08 10:58:04 -0700

    posting code behind post

---
+++ .ipynb_checkpoints/txt-checkpoint.py
@@ -0,0 +1,74 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from flask import Flask "
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      " * Serving Flask app \"__main__\" (lazy loading)\n",
+      " * Environment: production\n",
+      "   WARNING: Do not use the development server in a production environment.\n",
+      "   Use a production WSGI server instead.\n",
+      " * Debug mode: off\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      " * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)\n"
+     ]
+    }
+   ],
+   "source": [
+    "app = Flask(__name__) \n",
+    "\n",
+    "@app.route('/') \n",
+    "\n",
+    "def hello_world(): \n",
+    "    return 'Hello World'\n",
+    "\n",
+    "\n",
+    "if __name__ == '__main__': \n",
+    "  \n",
+    "    # run() method of Flask class runs the application  \n",
+    "    # on the local development server. \n",
+    "    app.run() "
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
